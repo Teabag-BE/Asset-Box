35bcfdf1e6f879033b4bde4294e4c1b843d50c0c
FA-50 <wjdtn747@naver.com>
2026-05-31 20:46:55 +0900

    [FEAT] OAuthService 기능 추가

---
+++ src/main/java/io/teabag/assetbox/user/repository/UserReposiotry.java
@@ -0,0 +1,17 @@
+package io.teabag.assetbox.user.repository;
+
+import io.teabag.assetbox.common.exception.BusinessException;
+import io.teabag.assetbox.common.exception.ErrorCode;
+import io.teabag.assetbox.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserReposiotry extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    default User findByEmailOrThrow(String email){
+        return findByEmail(email).orElseThrow(()-> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    }
+}
