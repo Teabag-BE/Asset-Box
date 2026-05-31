c96c01a08b8fe5969801356b48f9711c4488845b
FA-50 <wjdtn747@naver.com>
2026-05-31 16:31:47 +0900

    [CHORE] 공통 예외 클래스 수정 및 ErrorCode 수정

--- src/main/java/io/teabag/assetbox/common/exception/ErrorCode.java
+++ src/main/java/io/teabag/assetbox/common/exception/ErrorCode.java
@@ -1,27 +1,27 @@
 package io.teabag.assetbox.common.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public enum ErrorCode {
-    VALIDATION_FAILED(HttpStatus.BAD_REQUEST),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED),
-    FORBIDDEN(HttpStatus.FORBIDDEN),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND),
-    CATEGORY_DEPTH_INVALID(HttpStatus.BAD_REQUEST),
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND),
-    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND),
-    FILE_NOT_FOUND(HttpStatus.NOT_FOUND),
-    FEEDBACK_NOT_FOUND(HttpStatus.NOT_FOUND),
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR);
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "검증에 실패했습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증에 실패했습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "현재 계정에 권한이 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 계정을 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 카테고리를 찾을 수 없습니다."),
+    CATEGORY_DEPTH_INVALID(HttpStatus.BAD_REQUEST, "선택할 수 없는 카테고리입니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시글을 찾을 수 없습니다."),
+    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 요청 게시글을 찾을 수 없습니다."),
+    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 파일을 찾을 수 없습니다."),
+    FEEDBACK_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 피드백을 찾을 수 없습니다."),
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
     private final HttpStatus status;
+    private final String description;
 
-    ErrorCode(HttpStatus status) {
+    ErrorCode(HttpStatus status, String description) {
         this.status = status;
-    }
-
-    public HttpStatus status() {
-        return status;
+        this.description = description;
     }
 }
