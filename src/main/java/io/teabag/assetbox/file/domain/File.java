b28c8bdcc48e99def98dff6e926f1011606308f8
한승우 <tmddn_00@naver.com>
2026-06-03 17:10:42 +0900

    mod: File 도메인 contentType 필드에 대한 주석 추가

--- src/main/java/io/teabag/assetbox/file/domain/File.java
+++ src/main/java/io/teabag/assetbox/file/domain/File.java
@@ -44,8 +44,22 @@ public class File extends BaseEntity {
     @Column(nullable = false)
     private Long uploadOrder;
 
+
     @Column(length = 100)
     private String contentType;
+    /*
+    contentType은 브라우저나 S3, HTTP 응답에서 “이 파일을 어떻게 다룰지” 판단할 때 씁니다.
+    이미지 파일을 다운로드할 때 Content-Type이 image/png면 브라우저가 이미지로 인식합니다.
+    반대로 application/octet-stream이면 브라우저는 그냥 일반 바이너리 파일처럼 취급할 수 있습니다.
+    예시
+    image/png
+    image/jpeg
+    application/pdf
+    application/octet-stream
+    model/gltf-binary
+     */
+
+
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 30)
